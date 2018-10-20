dc97b9ea52a26499e7b8d9505b3b5d375ef7913b
Phillip Nguyen <pdnguye3@asu.edu>
2018-10-20 14:42:11 -0700

    Update Donor.java

--- Donor.java
+++ Donor.java
@@ -15,36 +15,36 @@ public class Donor
     {
 
         this.donationID = Integer.parseInt(arr[0]);
-	this.donorID = Integer.parseInt(arr[1]);
-	this.companyName = arr[2];
-	this.firstName = arr[3];
-	this.middleName = arr[4];
-	this.lastName = arr[5];
-	this.emailAddress = arr[6];
-	this.spouseName = arr[7];
-	this.greeting = arr[8];
-	this.streetAddress = arr[9];
-	this.apartment = arr[10];
-	this.city = arr[11];
-	this.state = arr[12];
-	this.zipCode = Integer.parseInt(arr[13]);
-	this.donorType = arr[14];
-	this.donationType = arr[15];
-	this.donationSource = arr[16];
-	this.donatedOn = arr[17];
-	this.foodCategory = arr[18];
-	this.foodName = arr[19];
-	this.quantity = Integer.parseInt(arr[20]);
-	this.quantityType = arr[21];
-	this.weight = Double.parseDouble(arr[21]);
-	this.value = Double.parseDouble(arr[22]);
-	this.memo = arr[24];
+				this.donorID = Integer.parseInt(arr[1]);
+				this.companyName = arr[2];
+				this.firstName = arr[3];
+				this.middleName = arr[4];
+				this.lastName = arr[5];
+				this.emailAddress = arr[6];
+				this.spouseName = arr[7];
+				this.greeting = arr[8];
+				this.streetAddress = arr[9];
+				this.apartment = arr[10];
+				this.city = arr[11];
+				this.state = arr[12];
+				this.zipCode = Integer.parseInt(arr[13]);
+				this.donorType = arr[14];
+				this.donationType = arr[15];
+				this.donationSource = arr[16];
+				this.donatedOn = arr[17];
+				this.foodCategory = arr[18];
+				this.foodName = arr[19];
+				this.quantity = Integer.parseInt(arr[20]);
+				this.quantityType = arr[21];
+				this.weight = Double.parseDouble(arr[21]);
+				this.value = Double.parseDouble(arr[22]);
+				this.memo = arr[24];
 
     }
 
     public int getDonationID()
     {
-	return donationID;
+				return donationID;
     }
 	
     public int getDonorID()
