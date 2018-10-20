fbcb39df828ee96bae5ef5289d6a956200188f8c
kwood14 <44320500+kwood14@users.noreply.github.com>
2018-10-20 14:28:11 -0700

    Update Donor.java
    Revised the donor constructor

--- Donor.java
+++ Donor.java
@@ -5,34 +5,33 @@ public class Donor
 	private int donationID, donorID, zipCode, quantity;
 	private double weight, value;
 	private String companyName, firstName, middleName, lastName, emailAddress, spouseName, greeting, streetAddress, apartment, city, state, donorType, donationType, donationSource, donatedOn, foodCategory, foodName, quantityType, memo;
-	public Donor(int donationID, int donorID, String companyName, String firstName, String middleName, String lastName, String emailAddress, String spouseName, String greeting, String streetAddress, String apartment, String city, String state, int zipCode, String donorType, String donationType, String donationSource, String donatedOn, String foodCategory, String foodName, int quantity, String quantityType, double weight, double value, String memo)
+	public Donor(String[] arr)
 	{
-		this.donationID = donationID;
-		this.donorID = donorID;
-		this.companyName = companyName;
-		this.firstName = firstName;
-		this.middleName = middleName;
-		this.lastName = lastName;
-		this.emailAddress = emailAddress;
-		this.spouseName = spouseName;
-		this.greeting = greeting;
-		this.streetAddress = streetAddress;
-		this.apartment = apartment;
-		this.city = city;
-		this.state = state;
-		this.zipCode = zipCode;
-		this.donorType = donorType;
-		this.donationType = donationType;
-		this.donationSource = donationSource;
-		this.donatedOn = donatedOn;
-		this.foodCategory = foodCategory;
-		this.foodName = foodName;
-		this.quantity = quantity;
-		this.quantityType = quantityType;
-		this.weight = weight;
-		this.value = value;
-		this.memo = memo;
-		
+		this.donationID = Integer.parseInt(arr[0]);
+		this.donorID = Integer.parseInt(arr[1]);
+		this.companyName = arr[2];
+		this.firstName = arr[3];
+		this.middleName = arr[4];
+		this.lastName = arr[5];
+		this.emailAddress = arr[6];
+		this.spouseName = arr[7];
+		this.greeting = arr[8];
+		this.streetAddress = arr[9];
+		this.apartment = arr[10];
+		this.city = arr[11];
+		this.state = arr[12];
+		this.zipCode = Integer.parseInt(arr[13]);
+		this.donorType = arr[14];
+		this.donationType = arr[15];
+		this.donationSource = arr[16];
+		this.donatedOn = arr[17];
+		this.foodCategory = arr[18];
+		this.foodName = arr[19];
+		this.quantity = Integer.parseInt(arr[20]);
+		this.quantityType = arr[21];
+		this.weight = Double.parseDouble(arr[21]);
+		this.value = Double.parseDouble(arr[22]);
+		this.memo = arr[24];
 	}
 	
 	public int getDonationID()
