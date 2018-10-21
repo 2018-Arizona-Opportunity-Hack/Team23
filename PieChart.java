bc5f493f82b98437e600f7f85859fc6deb4b8b53
kwood14 <44320500+kwood14@users.noreply.github.com>
2018-10-21 06:33:47 -0700

    Update PieChart.java
    turns background white

--- PieChart.java
+++ PieChart.java
@@ -1,9 +1,15 @@
 //package converter; // NetBeans IDE
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.File;
 
+import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 
 public class PieChart 
@@ -15,6 +21,7 @@ public class PieChart
 	{
 		color = color.black;
 		percent = 0;
+		name = "";
 	}
 	
 	public void setColor(Color color)
@@ -42,6 +49,11 @@ public class PieChart
 		return color;
 	}
 	
+	public String getName()
+	{
+		return name;
+	}
+	
 }
 class PieChartMaker extends JComponent
 {
@@ -50,29 +62,44 @@ class PieChartMaker extends JComponent
 		PieChart[] chart = new PieChart[num];
 		for(int i = 0; i < num; i++)
 		{
+			chart[i] = new PieChart();
 			chart[i].setColor(color[i]);
 			chart[i].setPercentage(percent[i]);
 			chart[i].setName(name[i]);
 		}
+		
 		return chart;
 	}
 	
 	public void drawChart(Graphics2D g, Rectangle a, PieChart[] chart)
 	{
+		int cY = a.y;
 		double sum = 0;
+		g.setColor(Color.white);
+		g.fill(a);
+		Font font = new Font("Serif", Font.BOLD, 25);
+		g.setFont(font);
 		for(int i = 0; i < chart.length; i++)
 		{
 			sum += chart[i].getPercent();
+			g.setColor(chart[i].getColor());
+			g.drawString(chart[i].getName(), a.x, cY + 18);
+			cY += 25;
 		}
 		double current = 0;
-		int angle, arcAngle;
+		int angle,arcAngle = 0;
+		cY = a.y;
 		for(int i = 0; i < chart.length; i++)
 		{
 			angle = (int)(current/sum * 360);
 			arcAngle = (int)(chart[i].getPercent()/sum *360);
-			g.setColor(chart[i].getColor());
+			g.setColor(chart[i].getColor()); 
 			g.fillArc(a.x, a.y, a.width, a.height, angle, arcAngle);
 			current += chart[i].getPercent();
+			double percentages = (double)(chart[i].getPercent()/sum*100);
+			g.drawString(String.format("%.2f", percentages) + "%", a.x + 700, cY + 18);
+			cY += 25;
 		}
 	}
 }
+
