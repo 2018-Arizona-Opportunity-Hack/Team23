fbd7748db39a9c7493b4f7cb845fb4e55ac715af
fillupwin <emilio.egnew@gmail.com>
2018-10-21 00:25:34 -0700

    Update Donor.java

--- Donor.java
+++ Donor.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package opportunity2018;
-
 /**
  * Donor. Organizes the attributes into their respective donors.
  * @author Phillip N, Emilio E, Kai W
